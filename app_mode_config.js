3b38c7bde4f5d19e13978a21a116cb18f91fabde
KJB <sk01197375068@gmail.com>
2026-08-17 15:50:39 +0900

    Load persistent wrong-learning foundation

--- app_mode_config.js
+++ app_mode_config.js
@@ -19,4 +19,15 @@
       adminStartsInDevConsole:false
     })
   });
+
+  const p=location.pathname||'';
+  const student=/\/fast_index\.html$/i.test(p)||/\/fast_day\.html$/i.test(p)||/\/adaptive_fast_review\.html$/i.test(p)||/\/korean_history2_day[1-6]_(?:student_flow_app|metacog_app)\.html$/i.test(p)||/\/korean_history2_unit1_adaptive_review_metacog_flow\.html$/i.test(p);
+  if(student&&!window.__H2_WRONG_SYSTEM_LOADER_REQUESTED__){
+    window.__H2_WRONG_SYSTEM_LOADER_REQUESTED__=true;
+    const s=document.createElement('script');
+    s.src='wrong_learning_system_bootstrap.js';
+    s.defer=true;
+    s.onerror=()=>console.error('[History2] wrong_learning_system_bootstrap.js load failed');
+    document.head.appendChild(s);
+  }
 })();
